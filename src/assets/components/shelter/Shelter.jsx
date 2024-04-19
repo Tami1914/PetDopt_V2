b6fa594ac58c54567533a3fcda550558faa78872
Tami1914 <aythami.1914@gmail.com>
2024-04-19 18:18:39 +0100

    animal shelter page

--- src/assets/components/shelter/Shelter.jsx
+++ src/assets/components/shelter/Shelter.jsx
@@ -6,23 +6,23 @@ function Shelter() {
     <>
       <div className="shelter-container">
         {
-            animalShelters.map((animalShelter, index) => (
-              <>
-            <div className="shelter-container-img" key={index}>
-              <img src={`/images/${animalShelter.fileName}`} alt="imagen" />
-            </div>
-
-            <div className="shelter-container-text">
-
-              <div className="shelter-text-tittle">
-                <h2>{animalShelter.tittle}</h2>
-                <p>{animalShelter.ubication}</p>
-                <p>{animalShelter.cases}</p>
+          animalShelters.map((animalShelter, index) => (
+            <div className="shelter-container-text-image">
+              <div className="shelter-container-img" key={index}>
+                <img src={`/images/${animalShelter.fileName}`} alt="imagen" />
               </div>
 
+              <div className="shelter-container-text">
+
+                <div className="shelter-text-tittle">
+                  <h2>{animalShelter.tittle}</h2>
+                  <p>{animalShelter.ubication}</p>
+                  <p>{animalShelter.cases}</p>
+                </div>
+
+              </div>
             </div>
-            </>
-            ))
+          ))
         }
       </div>
     </>
